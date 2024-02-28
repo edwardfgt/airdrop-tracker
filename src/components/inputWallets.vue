8453cb880de06394fd85ee698e66ffc69fde9f59
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-02-28 19:21:02 +0000

    Create basic text area that splits string into an array by new line

---
+++ src/components/inputWallets.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <textarea v-model="inputText"></textarea>
+      <button @click="handleSubmit">Submit</button>
+    </div>
+</template>
+  
+  
+<script>
+export default {
+  data() {
+    return {
+      inputText: '',
+      linesArray: []
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.linesArray = this.inputText.split('\n');
+      console.log(this.linesArray);
+    }
+  }
+}
+</script>
