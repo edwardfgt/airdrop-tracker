bb6661927fc86b33f892ea95b2d4bae36439736e
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-03-01 23:00:54 +0000

    Create function to call zksync block explorer api with inputted wallets

--- src/components/inputWallets.vue
+++ src/components/inputWallets.vue
@@ -1,23 +1,43 @@
 <template>
-    <div>
-      <textarea v-model="inputText"></textarea>
-      <button @click="handleSubmit">Submit</button>
-    </div>
+  <div>
+    <textarea v-model="inputText"></textarea>
+    <button @click="handleSubmit">Submit</button>
+  </div>
 </template>
-  
-  
+
 <script>
 export default {
   data() {
     return {
       inputText: '',
-      linesArray: []
+      walletData: {}
     };
   },
   methods: {
-    handleSubmit() {
-      this.linesArray = this.inputText.split('\n');
-      console.log(this.linesArray);
+    async fetchWalletData(wallet) {
+      const url = `https://block-explorer-api.mainnet.zksync.io/api?module=account&action=txlist&page=1&offset=100&sort=desc&endblock=99999999&startblock=0&address=${wallet}`;
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return await response.json();
+      } catch (error) {
+        console.error("Fetch error: ", error);
+      }
+    },
+    async handleSubmit() {
+      const linesArray = this.inputText.split('\n');
+      this.walletData = {};
+
+      for (const wallet of linesArray) {
+        if (wallet) {
+          const data = await this.fetchWalletData(wallet);
+          this.walletData[wallet] = data;
+        }
+      }
+
+      console.log(this.walletData);
     }
   }
 }
